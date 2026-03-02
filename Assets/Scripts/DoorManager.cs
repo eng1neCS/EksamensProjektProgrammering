11e04b906dbf9bc989150d2a724ea37a417caa71
eng1neCS <nicholasbirkebaek@gmail.com>
2026-03-02 13:49:18 +0100

    DoorManager opstart

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -2,21 +2,50 @@ using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
-    [SerializeField] new GameObject waterDoor;
-    [SerializeField]
+    [SerializeField] new GameObject DoorP1;
+    [SerializeField] new GameObject DoorP2;
 
-    bool isFinished;
+    [SerializeField] bool isDoorOpen = false;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+      
+        if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
+        {
+            //if (!isDoorOpen)
+            //{
+            //    OpenDoor();
+            //}
+            if (DoorP1 != null && collision.CompareTag("Player1"))
+            {
+                // Play Door 1 animation
+                Debug.Log("Player 1 entered the door trigger");
+            }
+            else if (DoorP2 != null && collision.CompareTag("Player2"))
+            {
+                // Play Door 2 animation
+                Debug.Log("Player 2 entered the door trigger");
+            }
+            
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    //private void OpenDoor()
+    //{
+    //    isDoorOpen = true;
+
+    //    if (/*/onCollisionEnter2D.CompareTag("Player1")*/)
+    //    {
+    //        //spil Door 1 animation
+    //    }
+    //    else if (/*OnTriggerEnter2D.CompareTag("Player2")*/)
+    //    {
+    //        //spil Door 2 animation
+    //    }
+    //}
+
+
 }
