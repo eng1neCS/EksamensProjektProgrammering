109194047de0e28f6c5a1d6002aa4f2804ebfcab
eng1neCS <nicholasbirkebaek@gmail.com>
2026-04-14 09:08:40 +0200

    update men ikke færdiggørelse af timer optimisering

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -5,15 +5,29 @@ using TMPro;
 
 public class timer : MonoBehaviour
 {
+    public GameObject Win_Canvas;
     [SerializeField] TextMeshProUGUI Timertext;
     float elapsedTime;
+    
+    bool winCanvasActive = false;
 
 
     void Update()
+    {
+        Invoke("Timer", 0f);
+        //Hvis wincanvas er aktiv, så stop timeren.
+        if (winCanvasActive == true)
+        {
+            CancelInvoke("Timer");
+        }
+    }
+
+    public void Timer()
     {
         elapsedTime += Time.deltaTime;
         int minutes = Mathf.FloorToInt(elapsedTime / 60);
         int seconds = Mathf.FloorToInt(elapsedTime % 60);
         Timertext.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
 }
