799c69730b902d6f4961888121313715ffc69e84
kochmeisterbeer <kasperkochghg@gmail.com>
2026-04-17 16:06:40 +0200

    water and lava scripts

--- Assets/Scripts/slime_and_lava.cs
+++ Assets/Scripts/slime_and_lava.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Water : MonoBehaviour
+public class Lava : MonoBehaviour
 {
 
-    
+    private bool player1cooked = false;
     [SerializeField] private string level;
     [SerializeField] private string playerID;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         
+        
             if (collision.GetComponent<MovementScript>().PlayerID == playerID)
             {
-               
-            SceneManager.LoadScene(level);
-        
+                 SceneManager.LoadScene(level);
             }
-            
 
-    
-       
+
+        
+     
+
+      
     }
 
-
-
-}
+   }
